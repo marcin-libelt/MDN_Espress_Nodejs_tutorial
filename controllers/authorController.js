142f0d9be6741e89c76b193eb294d4f6070a97bf
Marcin Libelt <m.libelt@alekseon.com>
2019-01-03 22:53:00 +0100

    created POST actions for authors, books, instances, genres

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,8 @@
 var Author = require('../models/author');
 var Book = require('../models/book');
 var async = require('async');
+var { body,validationResult } = require('express-validator/check');
+var { sanitizeBody } = require('express-validator/filter');
 
 exports.author_list = function(req, res, next) {
     Author.find()
@@ -41,13 +43,47 @@ exports.author_detail = function(req, res, next) {
 
 // Display Author create form on GET.
 exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+    res.render('author_form', { title: 'Create Author'});
 };
 
 // Handle Author create on POST.
-exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+    
+    body('first_name').isLength({ min: 1 }).trim().withMessage('First name must be specified.')
+    .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('family_name').isLength({ min: 1 }).trim().withMessage('Family name must be specified.')
+    .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601(),
+
+    sanitizeBody('first_name').trim().escape(),
+    sanitizeBody('family_name').trim().escape(),
+    sanitizeBody('date_of_birth').toDate(),
+    sanitizeBody('date_of_death').toDate(),
+
+    (req, res, next) => {
+    const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.render('author_form', { title: 'Create Author', author: req.body, errors: errors.array() });
+            return;
+        }
+        else {
+            var author = new Author(
+                {
+                    first_name: req.body.first_name,
+                    family_name: req.body.family_name,
+                    date_of_birth: req.body.date_of_birth,
+                    date_of_death: req.body.date_of_death
+                });
+            author.save(function (err) {
+                if (err) { return next(err); }
+                res.redirect(author.url);
+            });
+        }
+    }
+
+];
 
 // Display Author delete form on GET.
 exports.author_delete_get = function(req, res) {
--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,4 +1,7 @@
 var BookInstance = require('../models/bookinstance');
+var { body, validationResult } = require('express-validator/check');
+var { sanitizeBody } = require('express-validator/filter');
+var Book = require('../models/book');
 
 // Display list of all BookInstances.
 exports.bookinstance_list = function(req, res, next) {
@@ -25,14 +28,61 @@ exports.bookinstance_detail = function(req, res, next) {
 };
 
 // Display BookInstance create form on GET.
-exports.bookinstance_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance create GET');
+exports.bookinstance_create_get = function(req, res, next) {
+    Book.find({}, 'title')
+    .exec(function(err, books){
+        if(err) { return next(err)}
+        res.render('bookinstance_form', {title: 'Create Bookinstance', book_list: books })
+    });
 };
 
 // Handle BookInstance create on POST.
-exports.bookinstance_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance create POST');
-};
+exports.bookinstance_create_post = [
+    // Validate fields.
+    body('book', 'Book must be specified').isLength({ min: 1 }).trim(),
+    body('imprint', 'Imprint must be specified').isLength({ min: 1 }).trim(),
+    body('due_back', 'Invalid date').optional({ checkFalsy: true }).isISO8601(),
+    
+    // Sanitize fields.
+    sanitizeBody('book').trim().escape(),
+    sanitizeBody('imprint').trim().escape(),
+    sanitizeBody('status').trim().escape(),
+    sanitizeBody('due_back').toDate(),
+
+    (req, res, next) => {
+              
+        const errors = validationResult(req);
+
+        var bookinstance = new BookInstance({
+            book: req.body.book,
+            imprint: req.body.imprint,
+            status: req.body.status,
+            due_back: req.body.due_back
+        });
+
+        if(!errors.isEmpty()) {
+            Book.find({}, 'title')
+            .exec(function(err, books) {
+                if(err) { return next(err); }
+                res.render('bookinstance_form', { 
+                    title: 'Create Bookinstance', 
+                    book_list: books, 
+                    selected_book: bookinstance.book._id,
+                    errors: errors.array(),
+                    bookinstance: bookinstance  
+                })
+            });
+        } else {
+
+            bookinstance.save(function(err){
+                if(err) { return next(err); }
+                console.log('here?');
+                res.redirect(bookinstance.url);
+            })
+        }
+    }
+
+];
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = function(req, res) {
